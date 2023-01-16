d555da5a1b980736988344cdbb1a58e25b9214f9
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-01-16 12:25:17 +0100

    ..

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,14 +7,20 @@ import '../css/game.css'
 
 
 const Game = () => {
+  const {value, value2} = useContext(PokeContext)
+  const [data, setData] = value
+  const [isLoading, setIsLoading] = value2;
+
   const [playerHuman, setPlayerHuman] = useState("")
   const [playerCpu, setPlayerCpu] = useState("")
 
   return (
     <>
+    {data.result}
+    {isLoading && 
       <div className="game">
 
-      </div>
+      </div>}
     </>
   )
 }
