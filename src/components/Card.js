79b1048b4d853dabc6ead7b732be047d340c4dc2
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-01-17 01:41:36 +0100

    done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,46 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 
 const Card = ({ pokemon, loading, infoPokemon }) => {
-
-  const uniquePokemon = Array.from(new Set(pokemon.map(p => p.id))).map(id => {
-    return pokemon.find(p => p.id === id)
-  })
+  const uniquePokemon = Array.from(new Set(pokemon.map((p) => p.id))).map(
+    (id) => {
+      return pokemon.find((p) => p.id === id);
+    }
+  );
   // console.log(pokemon);
   return (
     <>
+    
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <Carousel className="caroussel">
+        
+        <Carousel className="caroussel" style={{width:'200px', display:'flex',justifyContent:'space-around'}}>
           {uniquePokemon.map((item) => {
             return (
-              <Carousel.Item key={item.id}>
-                <img className="imgcaroussel"
+              
+              <Carousel.Item key={item.id} >
+                <img
+                  className="imgcaroussel"
+                  
                   src={item.sprites.front_default}
                   alt=""
                 />
-                <Carousel.Caption>
+                <Carousel.Caption >
                   <h3>{item.name}</h3>
                   <p>{item.id}</p>
                   <p>
-                    <button className="buttoncaroussel" onClick={() => infoPokemon(item)}>Info</button>
+                    <button
+                      className="buttoncaroussel"
+                      onClick={() => infoPokemon(item)}
+                    >
+                      Info
+                    </button>
                   </p>
                 </Carousel.Caption>
+                
               </Carousel.Item>
+              
             );
           })}
         </Carousel>
